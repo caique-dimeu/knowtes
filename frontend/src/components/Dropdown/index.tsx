48310797f07030889f3e11e8be22bbe3cf4c7533
caique-dimeu <caique.dimeu@gmail.com>
2024-11-04 19:46:25 -0300

    não subir

--- frontend/src/components/Dropdown/index.tsx
+++ frontend/src/components/Dropdown/index.tsx
@@ -1,25 +1,12 @@
-import { DropdownContainer, OptionButton } from "./styles";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleHalfStroke, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
-import useLogout from "../../hooks/logout";
-import { useTheme } from "styled-components";
+// import { DropdownContainer, OptionButton } from "./styles";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCircleHalfStroke, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+// import useLogout from "../../hooks/logout";
+// import { useTheme } from "styled-components";
 
 export default function Dropdown() {
-    const { logout } = useLogout();
-    const { theme } = useTheme();
 
     return (
-        <DropdownContainer theme={theme}>
-            <span>Jhon Doe</span>
-            <hr />
-            <OptionButton theme={theme}>
-                <FontAwesomeIcon icon={faCircleHalfStroke} />
-                <span>Tema</span>
-            </OptionButton>
-            <OptionButton onClick={logout} theme={theme}>
-                <FontAwesomeIcon icon={faRightFromBracket} />
-                <span>Sair</span>
-            </OptionButton>
-        </DropdownContainer>
+        <></>
     );
 }
